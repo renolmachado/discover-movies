5c22bc22d62ba555692c8357e9c4c75771b5c278
Renol Machado <renol94@gmail.com>
2022-07-20 18:34:56 -0300

    Add DownArrow and Star icons

---
+++ src/components/icons/DownArrowIcon.tsx
@@ -0,0 +1,9 @@
+const DownArrowIcon = () => {
+  return (
+    <svg width="12" height="6" viewBox="0 0 12 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 -5.24537e-07L6 6L12 0" fill="white" />
+    </svg>
+  );
+};
+
+export default DownArrowIcon;
