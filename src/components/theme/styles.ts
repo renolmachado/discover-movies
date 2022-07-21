66df21ec114584d2ef46892d7480131e14a8572c
Renol Machado <renol94@gmail.com>
2022-07-20 21:55:28 -0300

    Add custom scroll

--- src/components/theme/styles.ts
+++ src/components/theme/styles.ts
@@ -6,5 +6,18 @@ export const GlobalStyle = createGlobalStyle`
     font-family: ${(props: { theme: ITheme }) => props.theme.fontFamily};
     font-weight: 400;
     font-style: normal;
+
+    &::-webkit-scrollbar {
+      width: 8px;
+      height: 5px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: #151515ab;
+      border-radius: 10px;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: #11171a;
+      border-radius: 10px;
+    }
   }
 `;
